f32e30eea6eb018b0e899bc0ed75d6811de87f2e
Cyril Ifenkwe <cyrilifenkwe@gmail.com>
2020-06-29 23:06:51 +0100

    Add solution to AbsoluteDifference

---
+++ AbsoluteDifference/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbsoluteDifference
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetResult(3));
+        }
+        public static int GetResult(int k)
+        {
+            int count = 0;
+            List<int> givenList = new List<int>{1,2};
+            List<int> difference = new List<int>();
+            int listLength = givenList.Count;
+            try
+            {
+                    while(count <  listLength - 1)
+                    {
+
+                        for(int i = count + 1;i < givenList.Count;i++)
+                        {
+                            int absoluteDifference = givenList[i] - givenList[count];
+                            if(givenList.LastIndexOf(absoluteDifference) <= -1)
+                            {
+                                givenList.Add(absoluteDifference); 
+                            }
+                        }
+                        givenList.Sort();                    
+                        count++;
+                    }
+
+                    givenList.Reverse();
+                    return givenList[k - 1];
+            }
+            catch(Exception ex)
+            {
+                return -1;
+            }
+        }
+
+            
+    }
+}
